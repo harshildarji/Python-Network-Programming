c5f0a099548d2471e8e5c69e731cd0296b832e24
Harshil <darjiharshil2994@gmail.com>
2018-01-24 16:59:20 +0530

    Chapter 01 6-7

---
+++ Chapter 01/7.py
@@ -0,0 +1,44 @@
+import sys, socket, argparse
+
+def main():
+    parser = argparse.ArgumentParser(description = 'Socket Error Example')
+    parser.add_argument('--host', action = 'store', dest = 'host', required = False)
+    parser.add_argument('--port', action = 'store', dest = 'port', type = int, required = False)
+    parser.add_argument('--file', action = 'store', dest = 'file', required = False)
+    given_args = parser.parse_args()
+    host = given_args.host
+    port = given_args.port
+    filename = given_args.file
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as e:
+        print('Error creating socket: %s' % e)
+        sys.exit(1)
+
+    try:
+        s.connect((host, port))
+    except socket.gaierror as e:
+        print('Address-related error connecting to server: %s' % e)
+        sys.exit(1)
+    except socket.error as e:
+        print('Connection error: %s' % e)
+
+    try:
+        msg = 'GET %s HTTP/1.0\r\n\r\n' % filename
+        s.sendall(msg.encode('utf-8'))
+    except socket.error as e:
+        print('Error sending data: %s' % e)
+        sys.exit(1)
+
+    while 1:
+        try:
+            buf = s.recv(2048)
+        except socket.error as e:
+            print('Error receiving data: %s' % e)
+            sys.exit(1)
+        if not len(buf): break
+        sys.stdout.write(buf.decode('utf-8'))
+
+if __name__ == '__main__':
+    main()
