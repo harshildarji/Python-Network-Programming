e993e3516395f2954a1191280d1bc12934bec964
Harshil <darjiharshil2994@gmail.com>
2018-01-27 12:42:45 +0530

    Chapter 01 13

---
+++ Chapter 01/13_Server.py
@@ -0,0 +1,29 @@
+import socket, sys, argparse
+
+host = 'localhost'
+data_payload = 2048
+backlog = 5
+
+def echo_server(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_address = (host, port)
+    print('Starting up echo server on %s port %s' % server_address)
+    sock.bind(server_address)
+    sock.listen(backlog)
+    while True:
+        print('Waiting to receive message from client')
+        client, address = sock.accept()
+        data = client.recv(data_payload)
+        if data:
+            print('Data:', data.decode())
+            client.send(data)
+            print('sent %s back to %s' % (data.decode(), address))
+        client.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = 'Socket Server Example')
+    parser.add_argument('--port', action = 'store', dest = 'port', type = int, required = True)
+    given_args = parser.parse_args()
+    port = given_args.port
+    echo_server(port)
